7a7b60bebabdd99ee797849d93944acd9723eea7
Michael <mpone1717@gmail.com>
2025-01-14 01:42:15 -0500

    added for loop to create grid
    added variables to give the grid size and cell size.

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-!
+
+
+const cdiv = document.querySelector("#container")
+const gridSize = 16;
+const cellSize = 40;
+
+function createGrid(size){
+    for(let i = 0; i < size * size; i++){
+      const cell = document.createElement("div") 
+      cell.style.width = `${cellSize}px`
+      cell.style.height = `${cellSize}px`
+      cell.style.border = "2px solid"
+    cdiv.appendChild(cell)
+    }
+}
+    createGrid(gridSize)
